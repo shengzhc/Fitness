a81daa642f460a47357c13d2ad60ddda35ab37f8
shengzhc <shengzhc@usc.edu>
2013-09-22 19:03:07 -0700

    update

---
+++ FitnessAssistant/FitnessAssistant/FAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FAAppDelegate.h
+//  FitnessAssistant
+//
+//  Created by Shengzhe Chen on 9/22/13.
+//  Copyright (c) 2013 iBros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FitnessAssistant/FitnessAssistant/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FitnessAssistant
+//
+//  Created by Shengzhe Chen on 9/22/13.
+//  Copyright (c) 2013 iBros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FAAppDelegate class]));
+    }
+}
